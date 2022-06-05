e85201db82af2df1f9329294afc7486a8f1a04fe
Aziz Kilic <kaneda942@gmail.com>
2022-06-05 16:47:24 +0200

    Footer Problems 01

--- frontend/layouts/Footer.vue
+++ frontend/layouts/Footer.vue
@@ -5,15 +5,15 @@
         <h3 class="text-center text-xl font-bold text-primary bg-secondary">hit me up</h3>
 
         <div class="flex content-center justify-center mt-6 pb-32">
-          <a class="mr-6" href="https://www.xing.com/profile/Aziz_Kilic11/" target="_blank" rel="noopener noreferrer">
+          <a class="mr-6" href="https://www.xing.com/profile/Aziz_Kilic11/" target="_blank" rel="no">
             <img class="" src="~/assets/images/xing.svg" width="32" height="32" uk-svg />
           </a>
 
-          <a class="mr-6" href="mailto:azizkilic942@gmail.com" target="_blank" rel="noopener noreferrer">
+          <a class="mr-6" href="mailto:azizkilic942@gmail.com" target="_blank" rel="noopener norefe">
             <img class="" src="~/assets/images/email.svg" width="32" height="32" uk-svg />
           </a>
 
-          <a class="" href="https://www.linkedin.com/in/aziz-kilic-97a3aa21b/" target="_blank" rel="noopener noreferrer">
+          <a class="" href="https://www.linkedin.com/in/aziz-kilic-97a3aa21b/" target="_blank" rel=>
             <img class="" src="~/assets/images/LinkedIn.svg" width="32" height="32" uk-svg />
           </a>
 
